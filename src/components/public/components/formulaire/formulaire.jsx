237eb142244c8c303df34fe38853ff4ae6f88694
FilleDeZeus <enide26@hotmail.fr>
2023-03-17 15:07:48 +0100

    ajout condition

--- src/components/public/components/formulaire/formulaire.jsx
+++ src/components/public/components/formulaire/formulaire.jsx
@@ -13,10 +13,16 @@ export const Formulaire = () => {
     // etat du user correspond un objet 
     const [user, setUser] = useState({ nom: '', prenom: '', age: '' });
 
+    const [erreur, setErreur] = useState('');
     // fct qui valider le formulaire
     const valider = (e) => {
         // annule le rechargement de la page
         e.preventDefault();
+        // si un des champs pas rempli
+        if (!user.nom || !user.prenom || !user.age) {
+            setErreur(`Tous les champs n'ont pas été remplis `);
+            return;
+        }
         // elle cree à new user
         const newUser = {
             id: Date.now(), // avec un id unique
@@ -26,7 +32,11 @@ export const Formulaire = () => {
         store.dispatch(formulaireSlice.actions.ajouter(newUser));
         // mis a jour de l'etat User
         setUser({ nom: '', prenom: '', age: '' });
-};
+    };
+    // Vérifier si erreur existe et effacer si tous les champs sont remplis
+    if (erreur && user.nom && user.prenom && user.age) {
+        setErreur('');
+    }
     // fct qd les input form change
     const changer = (e) => {
         // recupere 
@@ -34,16 +44,27 @@ export const Formulaire = () => {
         // met à jour l'etat User en ajoutant le new user au ancien
         setUser((prevData) => ({ ...prevData, [name]: value }));
     };
+    
 
     return (
-        <form onSubmit={valider}>
-            <label htmlFor="nom">Nom:</label>
-            <input type="text" id="nom" name="nom" value={user.nom} onChange={changer} />
-            <label htmlFor="prenom">Prénom:</label>
-            <input type="text" id="prenom" name="prenom" value={user.prenom} onChange={changer} />
-            <label htmlFor="age">Âge:</label>
-            <input type="number" id="age" name="age" value={user.age} onChange={changer} />
-            <button type="submit">Ajouter</button>
-        </form>
+        <div className='formulaire'>
+            <form onSubmit={valider}>
+                <label htmlFor="nom">
+                    Nom:
+                    <input type="text" id="nom" name="nom" value={user.nom} onChange={changer} />
+                </label>
+                <label htmlFor="prenom">
+                    Prénom:
+                    <input type="text" id="prenom" name="prenom" value={user.prenom} onChange={changer} />
+                </label>
+                <label htmlFor="age">
+                    Âge:
+                    <input type="number" id="age" name="age" value={user.age} onChange={changer} />
+                </label>
+                <button type="submit">Ajouter</button>
+            </form>
+            {erreur && <span className='erreur'>{erreur}</span>}
+        </div>
+        
     );
 };
