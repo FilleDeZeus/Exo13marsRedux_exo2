67554774c1919ea0f433f04cf20018cbaf9f61cb
FilleDeZeus <enide26@hotmail.fr>
2023-03-17 15:23:44 +0100

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { Page } from './components/page.jsx';
 import './App.scss'
-import {store} from './store.jsx';
-import { Provider } from 'react-redux';
 
 export const App = () => {
   return (
-    <Provider store={store}>
     <div className='app'>
       <Page />
     </div>
-    </Provider>
 
   );
 };
--- src/appContext/appContextProvider.jsx
+++ src/appContext/appContextProvider.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 // importation de appContext
 import { AppContext } from './appContext';
 // importation du store
@@ -7,9 +8,11 @@ import {store} from '../store.jsx';
 // creation et exportation du appContextProvider 
 export const AppContextProvider = ({ children }) => {
     return (
-        
+    <Provider store={store}>
         <AppContext.Provider value={{ store }}>
             {children}
         </AppContext.Provider>
+    </Provider>
+
     );
 };
